7ae5a3290daf722d773630a0b17911d9c824db4a
Gordon-Frohman <sanisimov6699@gmail.com>
2024-02-18 16:42:22 +0700

    Projectiles traits tuning
    Raven feather arrows now do not show name of the killer in chat and also should not provoke mobs (since they don't know what hit them)
    Knightmetal projectiles now provide resistance as well

--- src/main/java/tconstruct/tools/TFToolEvents.java
+++ src/main/java/tconstruct/tools/TFToolEvents.java
@@ -2,11 +2,18 @@ package tconstruct.tools;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EntityDamageSourceIndirect;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
@@ -22,6 +29,8 @@ import tconstruct.weaponry.entity.BoltEntity;
 import twilightforest.integration.TFTinkerConstructIntegration.MaterialID;
 
 public class TFToolEvents {
+
+    Random random = new Random();
 
     @SubscribeEvent
     public void craftTool(ToolCraftEvent.NormalTool event) {
@@ -98,20 +107,33 @@ public class TFToolEvents {
                 || !event.itemStack.getTagCompound().getCompoundTag("InfiTool").hasKey("IsInTF"))
             return;
 
-        event.toolTip.add(
-                1,
-                "" + colorFromID(event.itemStack.getTagCompound().getCompoundTag("InfiTool").getInteger("TwilitID"))
-                        + StatCollector.translateToLocal("material.twilit.ability"));
+        NBTTagCompound tags = event.itemStack.getTagCompound().getCompoundTag("InfiTool");
+        int tooltipIndex = 1;
+        int lastEmptyIndex = -1;
+        int twilitID = tags.getInteger("TwilitID");
+        ChatFormatting color = colorFromID(twilitID);
+        if (twilitID == 5) {
+            int x = 0;
+        }
+        for (int i = event.toolTip.size() - 1; i > 1; i--) {
+            if (event.toolTip.get(i).contains(color.toString())) tooltipIndex = i;
+            if (event.toolTip.get(i).isEmpty() && !event.toolTip.get(i - 1).isEmpty()) lastEmptyIndex = i;
+        }
+        if (lastEmptyIndex == -1) lastEmptyIndex = event.toolTip.size();
+        if (twilitID == 5) tooltipIndex = lastEmptyIndex;
+
+        event.toolTip.add(tooltipIndex, "" + color + StatCollector.translateToLocal("material.twilit.ability"));
         if (event.itemStack.getItem() instanceof ArrowAmmo || event.itemStack.getItem() instanceof BoltAmmo)
-            if (event.itemStack.getTagCompound().getCompoundTag("InfiTool").getInteger("Accessory") == 5)
-                event.toolTip.add(
-                        2,
-                        "" + ChatFormatting.DARK_GRAY
-                                + StatCollector.translateToLocal("material.raven_feather.ability"));
-        if (event.itemStack.getTagCompound().getCompoundTag("InfiTool").hasKey("TFFiery")) {
-            event.toolTip
-                    .add(2, "" + ChatFormatting.GOLD + StatCollector.translateToLocal("modifier.tooltip.Auto-Smelt"));
-            event.toolTip.add(3, "" + ChatFormatting.GOLD + StatCollector.translateToLocal("modifier.tooltip.Fiery"));
+            if (tags.getInteger("Accessory") == 5) event.toolTip.add(
+                    lastEmptyIndex + 1,
+                    "" + ChatFormatting.DARK_GRAY + StatCollector.translateToLocal("material.raven_feather.ability"));
+        if (tags.hasKey("TFFiery")) {
+            event.toolTip.add(
+                    tooltipIndex + 1,
+                    "" + ChatFormatting.GOLD + StatCollector.translateToLocal("modifier.tooltip.Auto-Smelt"));
+            event.toolTip.add(
+                    tooltipIndex + 2,
+                    "" + ChatFormatting.GOLD + StatCollector.translateToLocal("modifier.tooltip.Fiery"));
         }
     }
 
@@ -144,6 +166,54 @@ public class TFToolEvents {
             if (accessory == 5) {
                 entity.setInvisible(true);
                 entity.renderDistanceWeight = 0;
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onProjectileHit(LivingHurtEvent event) {
+        if (event.source instanceof EntityDamageSourceIndirect) {
+            EntityDamageSourceIndirect damageSource = (EntityDamageSourceIndirect) event.source;
+            if (damageSource.damageSourceEntity instanceof ProjectileBase) {
+                ProjectileBase entity = (ProjectileBase) damageSource.damageSourceEntity;
+                ItemStack projectile = entity.getEntityItem();
+                if (projectile.getTagCompound().getCompoundTag("InfiTool").hasKey("Stalwart")) {
+                    if (damageSource.indirectEntity instanceof EntityLivingBase && random.nextInt(10) == 0) {
+                        PotionEffect potionEffect = ((EntityLivingBase) damageSource.indirectEntity)
+                                .getActivePotionEffect(Potion.resistance);
+                        int amplifier = 0;
+                        int duration = 200;
+                        if (potionEffect != null) {
+                            amplifier = (potionEffect.getAmplifier() < 2) ? potionEffect.getAmplifier() + 1 : 2;
+                            duration = (potionEffect.getDuration() < 401) ? potionEffect.getDuration() + 200 : 600;
+                        }
+                        ((EntityLivingBase) damageSource.indirectEntity)
+                                .addPotionEffect(new PotionEffect(Potion.resistance.id, duration, amplifier));
+                    }
+                }
+                if (damageSource.damageSourceEntity instanceof ArrowEntity
+                        || damageSource.damageSourceEntity instanceof BoltEntity) {
+                    int accessory = projectile.getTagCompound().getCompoundTag("InfiTool").getInteger("Accessory");
+                    if (accessory == 5) {
+                        damageSource.indirectEntity = null;
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onProjectileKill(LivingDeathEvent event) {
+        if (event.source instanceof EntityDamageSourceIndirect) {
+            EntityDamageSourceIndirect damageSource = (EntityDamageSourceIndirect) event.source;
+            if (damageSource.damageSourceEntity instanceof ArrowEntity
+                    || damageSource.damageSourceEntity instanceof BoltEntity) {
+                ProjectileBase entity = (ProjectileBase) damageSource.damageSourceEntity;
+                int accessory = entity.getEntityItem().getTagCompound().getCompoundTag("InfiTool")
+                        .getInteger("Accessory");
+                if (accessory == 5) {
+                    damageSource.indirectEntity = null;
+                }
             }
         }
     }
