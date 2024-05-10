311fa6540ab1217c3ca2f795743143840ff8ec74
Gordon-Frohman <sanisimov6699@gmail.com>
2024-05-10 11:01:02 +0700

    Remove stuff from another PR

--- src/main/java/twilightforest/TwilightForestMod.java
+++ src/main/java/twilightforest/TwilightForestMod.java
@@ -360,10 +360,7 @@ public class TwilightForestMod {
             TFNeiIntegration.hideItem(new ItemStack(TFBlocks.doorTime));
             TFNeiIntegration.hideItem(new ItemStack(TFBlocks.doorTrans));
             TFNeiIntegration.hideItem(new ItemStack(TFBlocks.doorTwilight));
-            TFNeiIntegration.hideItem(new ItemStack(TFBlocks.firefly));
-            TFNeiIntegration.hideItem(new ItemStack(TFBlocks.cicada));
-            TFNeiIntegration.hideItem(new ItemStack(TFBlocks.moonworm));
-            for (int i = 0; i < 16; i++) TFNeiIntegration.hideItem(new ItemStack(TFBlocks.nagastone, 1, i));
+            for (int i = 0; i < 16; i++) TFNeiIntegration.hideItem(new ItemStack(TFBlocks.oldNagastone, 1, i));
         }
 
         // final check for biome ID conflicts
