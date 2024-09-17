66c89ed8fe52ab982431720a04b49bf29c5ab0c2
GitHub GTNH Actions <>
2024-09-17 22:26:10 +0000

    spotlessApply

--- src/main/java/twilightforest/block/BlockTFThorns.java
+++ src/main/java/twilightforest/block/BlockTFThorns.java
@@ -3,7 +3,6 @@ package twilightforest.block;
 import java.util.List;
 import java.util.Random;
 
-import cpw.mods.fml.common.Loader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.material.Material;
@@ -20,6 +19,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import twilightforest.TwilightForestMod;
@@ -39,10 +39,9 @@ public class BlockTFThorns extends BlockRotatedPillar {
         this.setNames(new String[] { "brown", "green" });
 
         this.setHardness(50.0F);
-        if (Loader.isModLoaded("dreamcraft")){
+        if (Loader.isModLoaded("dreamcraft")) {
             this.setResistance(10.0F);
-        }
-        else{
+        } else {
             this.setResistance(2000.0F);
         }
         this.setStepSound(soundTypeWood);
