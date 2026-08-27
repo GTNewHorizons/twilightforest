b5e64def40b26b87c7c9fddd2aff921234567c9b
DreamYao520 <123301307+DreamYao520@users.noreply.github.com>
2026-08-28 01:30:20 +0800

    Describe Timewood Clock controls

--- src/main/java/twilightforest/item/ItemBlockTFTree.java
+++ src/main/java/twilightforest/item/ItemBlockTFTree.java
@@ -1,7 +1,14 @@
 package twilightforest.item;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemBlockTFTree extends ItemBlockTFMeta {
 
@@ -13,5 +20,15 @@ public class ItemBlockTFTree extends ItemBlockTFMeta {
     public String getUnlocalizedName(ItemStack itemstack) {
         int meta = itemstack.getItemDamage() & 3;
         return super.getUnlocalizedName() + "." + meta;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> list, boolean advanced) {
+        super.addInformation(stack, player, list, advanced);
+        if ((stack.getItemDamage() & 3) == 0) {
+            list.add(StatCollector.translateToLocal("tile.TFMagicLogSpecial.0.tooltip.redstone"));
+            list.add(StatCollector.translateToLocal("tile.TFMagicLogSpecial.0.tooltip.softMallet"));
+        }
     }
 }
