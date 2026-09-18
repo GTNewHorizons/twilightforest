e3ebe95b2239df6444a8dea077afda471ebba92e
jgg <263927631+nullorator@users.noreply.github.com>
2026-09-19 01:36:56 +0200

    re-add accidentally deleted lines about magicLog and magicLeaves

--- src/main/java/twilightforest/item/TFRecipes.java
+++ src/main/java/twilightforest/item/TFRecipes.java
@@ -26,9 +26,11 @@ public class TFRecipes {
 
         // ore dictionary
         OreDictionary.registerOre("logWood", new ItemStack(TFBlocks.log, 1, OreDictionary.WILDCARD_VALUE));
+        OreDictionary.registerOre("logWood", new ItemStack(TFBlocks.magicLog, 1, OreDictionary.WILDCARD_VALUE));
         OreDictionary.registerOre("slabWood", new ItemStack(TFBlocks.woodenSlab, 1, OreDictionary.WILDCARD_VALUE));
         for (int i = 0; i < 10; i++) OreDictionary.registerOre("treeSapling", new ItemStack(TFBlocks.sapling, 1, i));
         OreDictionary.registerOre("treeLeaves", new ItemStack(TFBlocks.leaves, 1, OreDictionary.WILDCARD_VALUE));
+        OreDictionary.registerOre("treeLeaves", new ItemStack(TFBlocks.magicLeaves, 1, OreDictionary.WILDCARD_VALUE));
         OreDictionary.registerOre("plankWood", new ItemStack(TFBlocks.towerWood, 1, OreDictionary.WILDCARD_VALUE));
         for (int i = 0; i < 8; i++)
             OreDictionary.registerOre("plankWood", new ItemStack(TFBlocks.planks, i, OreDictionary.WILDCARD_VALUE));
